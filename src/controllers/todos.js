45dec60575da6662816c4cbdf771893ff8437ee3
Juan Lopez <juan@MacBook-Pro-de-Juan.local>
2020-08-31 14:30:54 +0100

    services and controllers created

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -12,31 +12,108 @@ export const getAll = async (req, res, next) => {
       count: todos.length
     })
   } catch (err) {
-    throw Error('Something happend');
+    next(Error('Something happend'))
   }
 
 }
 
 export const getOne = async (req, res, next) => {
-  res.status(200).json({
-    message: 'todos routes'
-  })
+
+  try {
+    const _id = req.params.id;
+
+    const todo = await TodoService.prototype.getTodoById(_id);
+
+    if(!todo) {
+      return res.status(404).json({
+        status: 'NOT FOUND ',
+        message: 'Todo not found...',
+      })
+    }
+
+    res.status(200).json({
+      status: 'OK',
+      message: 'Fetched Successfully...',
+      data: todo,
+    })
+  } catch (err) {
+
+    if(err.name === 'CastError'){
+      const status = 401;
+      const error = new Error(err.message);
+      error.status = status;
+      next(error)
+    }
+
+    next(err);
+  }
+
 }
 
 export const updateOne = async (req, res, next) => {
-  res.status(200).json({
-    message: 'todos routes'
-  })
+  try {
+    const _id = req.params.id;
+    const completed = req.body.completed
+
+    const todo = await TodoService.prototype.updateTodo(_id, completed)
+
+    if(todo.n === 0) {
+      return res.status(404).json({
+        status: 'NOT FOUND ',
+        message: 'Todo not found...',
+      })
+    }
+    res.status(200).json({
+      status: 'OK',
+      message: 'Todo succesfully updated ...',
+    })
+  } catch (err) {
+    next(err);
+  }
 }
 
 export const deleteOne = async (req, res, next) => {
-  res.status(200).json({
-    message: 'todos routes'
-  })
+  try {
+    const _id = req.params.id;
+    const todo = await TodoService.prototype.deleteTodo(_id)
+
+    if(todo.n === 0) {
+      return res.status(404).json({
+        status: 'NOT FOUND ',
+        message: 'Todo not found...',
+      })
+    }
+    res.status(200).json({
+      status: 'OK',
+      message: 'Todo succesfully deleted ...',
+    })
+  } catch (err) {
+    next(err);
+  }
 }
 
 export const create = async (req, res, next) => {
-  res.status(200).json({
-    message: 'todos routes'
+
+ try {
+  const title = req.body.title;
+  const description = req.body.description;
+
+  const newTodo = await TodoService.prototype.createTodo({title, description});
+
+  res.status(201).json({
+    status: 'CREATED',
+    message: 'Todo successfully created...',
+    data: {
+      _id: newTodo._id,
+      title: newTodo.title,
+      description: newTodo.description,
+      completed: newTodo.completed,
+      createdAt: newTodo.createdAt,
+      updatedAt: newTodo.updatedAt,
+    }
   })
+
+ } catch (err) {
+   next(err)
+ }
 }
--- src/services/Todos.js
+++ src/services/Todos.js
@@ -7,7 +7,35 @@ export default class TodoService {
     const todos = await Todos
       .find({})
       .select('-__v')
-      
+
     return todos
+  }
+
+  async getTodoById(id) {
+
+    const todo = await Todos
+    .findById(id)
+    .select('-__v');
+
+    return todo
+  }
+
+  async createTodo(todo) {
+    const newTodo = await Todos.create({
+      title: todo.title,
+      description: todo.description
+    })
+
+    return newTodo;
+  }
+
+  async deleteTodo(id,) {
+    const todo = await Todos.deleteOne({_id: id});
+    return todo;
+  }
+
+  async updateTodo(id, _completed) {
+    const todo = await Todos.updateOne({_id: id}, {completed: _completed});
+    return todo;
   }
 }
